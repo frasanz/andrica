0ba0d72130cf9efb65934e59c396d3394462be98
Francisco Sanz <frasanz@bifi.es>
2016-05-30 13:53:08 +0200

    running in linux64

--- Makefile
+++ Makefile
@@ -1,13 +1,20 @@
 WARNINGS=-Wall -Wextra
-OPTS=-O3 -g  -pedantic -static
+OPTS=-O4 -static
 EXE=gapdrica_l64
 
 LIB=-lgmp
 
+##Libraries BOINC
+BOINC_HOME= ${HOME}/boinc/boinc_build/boinc_7.7.0_l64
+BOINC      = -lboinc_api -lboinc
+INCLUDES   = -I$(BOINC_HOME)/lib -I$(BOINC_HOME)/api
+LIBRARIES  = -L$(BOINC_HOME)/lib -L$(BOINC_HOME)/api
+
+
 all: ${EXE}
 
 ${EXE} : gapdrica.c
-	${CC} -o ${EXE} gapdrica.c ${LIB}
+	${CXX} -o ${EXE} gapdrica.c ${INCLUDES} ${LIBRARIES} ${BOINC} ${LIB} ${OPTS}  libstdc++.a -pthread
 clean:
 		rm -f *.o
 		rm ${EXE}
--- gapdrica.c
+++ gapdrica.c
@@ -1,43 +1,69 @@
 #include <gmp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "boinc_api.h"
+#include "gui_rpc_client.h"
+#include "filesys.h"
+#include "diagnostics.h"
+
+#define OUTPUT_FILENAME "output"
 
 int main (void) {
+		 boinc_init();
+		 mpz_t init;    // The initial number
+     mpz_t p;       // To store the prime
+		 mpz_t p_ant;   // To store the previous prime
+		 mpf_t psq;     // To store the sqrt of the prime
+		 mpf_t p_antsq; // To store the sqrt of the previous prime
+		 mpz_t gap;     // To store the gap
+		 mpf_t andrica; // To store the andrica
 
-     mpz_t p;
-		 mpz_t p_ant;
-		 mpf_t psq;
-		 mpf_t p_antsq;
-		 mpz_t gap;
-		 mpf_t andrica;
-		 mpz_t comp1;
-		 mpz_t comp2;
+		 /* Some numbers to compare */
+		 mpz_t comp;
+		 mpz_t window;
+		 mpz_t upperbound;
+
+		 /* Some aux */
+		 mpz_t aux;
+		 mpf_t auxf;
+		 mpf_t windowf;
+
+		 FILE *fo;
+     char output_path[512];
+     boinc_resolve_filename(OUTPUT_FILENAME, output_path, sizeof(output_path));
 
 
-     // p+1 is NOT prime !!!
-     //mpz_init_set_str(p, "189452997113368438678230", 10);
-		 mpz_init_set_str(p,"1425172824437699411", 10);
-		 //mpz_init_set_str(p, "1", 10);
+		 /* Initialization */
+		 mpz_init_set_str(init,"4000000000000000000",10);
+		 mpz_init(p);
+		 mpz_set(p, init);
 
-		 mpz_init_set_str(comp1,"500", 10);
-		 mpz_init_set_str(comp2,"2", 10);
+		 /* Show if gap is greather than */
+		 mpz_init_set_str(comp,"500", 10);
 
+		 /* Doing up to */
+		 mpz_init_set_str(window,"500000000", 10);
+		 mpz_init(upperbound);
+		 mpz_add(upperbound, window, p);
 
-
-		 //mpz_init_set_str(p, "1", 10);
 		 mpz_init(p_ant);
 		 mpz_init(gap);
 
 		 mpf_init(psq);
 		 mpf_init(p_antsq);
 		 mpf_init(andrica);
-
 		 mpf_set_z(psq,p);
 		 mpf_sqrt(psq,psq);
-     while (1) {
+		 mpz_init(aux);
+		 mpf_init(windowf);
+		 mpf_init(auxf);
+
+     while (mpz_cmp(upperbound,p)>0) {
 				 mpz_set(p_ant,p);
          mpz_nextprime(p, p);
 
 				 mpz_sub(gap,p,p_ant);
-				 if(mpz_cmp(gap,comp1)>0){
+				 if(mpz_cmp(gap,comp)>0){
 					/* Gap is greater than comp 
 					 * doing andrica */
 				  mpf_set_z(p_antsq,p_ant);
@@ -47,13 +73,27 @@ int main (void) {
 				 	mpf_sqrt(psq,psq);
 
 				  mpf_sub(andrica,psq,p_antsq);
-				 	gmp_printf("%Zd %Zd %.20Ff\n",p_ant, gap, andrica);
+
+					/* Open boinc output file */
+					boinc_resolve_filename(OUTPUT_FILENAME, output_path, sizeof(output_path));
+					fo = boinc_fopen(output_path, "a+");
+					if(!fo) {
+						fprintf(stderr,"Couln't find output file, resolved name %s.\n", output_path);
+						exit(-1);
+					}
+				 	gmp_fprintf(fo,"%Zd %Zd %.20Ff\n",p_ant, gap, andrica);
+          fclose(fo);
+
+					/* Calculate the percentage */
+					mpz_sub(aux,p,init);
+					mpf_set_z(auxf, aux);
+					mpf_set_z(windowf, window);
+					mpf_div(auxf, auxf, windowf);
+					gmp_printf("%.3Ff\n",auxf);
+					boinc_fraction_done(mpf_get_d(auxf));
 				 }
-			   //if(mpz_cmp(gap,comp2)==0){
-				 //	gmp_printf("twin %Zd, %Zd, %.20Ff\n",p_ant, gap, andrica);
-				 //}
-
      }
-
+		 gmp_printf("%Zd\n",p);
+		 boinc_finish(0);
      return 0;
 }
